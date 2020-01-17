b5c3994b2ac5ec1cad646ee78dcbd3e80aca5cca
rajputroyal <46523182+rajputroyal@users.noreply.github.com>
2020-01-17 12:52:20 +0530

    Add files via upload

---
+++ divy 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   int c;
+	printf("/n1 for JAN,/n2 for FEB,/n3 for MARCH,/n4 for APRIL,/n5 for MAY,/n6 for JUNE,/n7 for JULY,/n8 for AUGUST,/n9 for SETP,/n10 for OCT,/n11 for NOV,/n12 for DEC");
+	switch(c)
+	{
+		case 1:
+		 	{
+			   printf("First month of the calendar is JAN");break;
+            }
+        case 2:
+		    {
+		       printf("Second month of the calendar is FED");break;
+			}
+		case 3:
+		    {
+			   printf("Third month of the calender is may");break;
+					}	    
+	}
+}
